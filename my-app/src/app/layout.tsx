87e38befdd827ee1c3ecbc102eed6f387e7818b9
Shakir Hussain <shakir.hussain6969@gmail.com>
2026-01-12 11:43:27 +0500

    added nav-filler for navbar fixed behavior

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
+        <div className="nav-filler"></div>
         {children}
         <Footer />
       </body>
