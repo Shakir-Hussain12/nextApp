e9c1c93c701c4a075fdfe969490f378037a060d1
Shakir Hussain <shakir.hussain6969@gmail.com>
2026-01-12 11:22:24 +0500

    Added and Styled Footer and Navbar

---
+++ my-app/src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <h5>Copyrighted...All Rights Reserved.</h5>
+        <div>
+          Links here....
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer;
