d529c9dd02c08ecf4bd771267ba504183429c908
Shakir Hussain <shakir.hussain6969@gmail.com>
2026-01-18 11:31:31 +0500

    added active page indicators

--- my-app/src/components/navbar/Navbar.tsx
+++ my-app/src/components/navbar/Navbar.tsx
@@ -1,14 +1,17 @@
+"use client";
 import Link from 'next/link';
 import React from 'react'
 import { links } from '@/data/data';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <div className="nav">
       <Link href="/">Logo</Link>
       <div>
         {
-          links?.map(link => <Link href={link.url} key={link.id} className='nav-item'>
+          links?.map(link => <Link href={link.url} key={link.id} className={`nav-item ${pathname === link.url ? 'active' : ''}`}>
               {link.title}
             </Link>)
         }
