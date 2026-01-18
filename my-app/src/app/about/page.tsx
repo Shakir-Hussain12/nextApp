fd411f73460d6e144b5fdf0cdc2533411ff895fc
Shakir Hussain <shakir.hussain6969@gmail.com>
2026-01-18 12:24:58 +0500

    completed About page

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
 
 const About = () => {
   return (
-    <>
-      About
-    </>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src='https://www.impactbnd.com/hubfs/blog-image-uploads/best-about-us-pages.jpg'
+          alt="About Image"
+          fill
+        ></Image>
+      </div>
+      <div className={styles.txtContainer}>
+        <div>
+          <h1>This is first text</h1>
+        </div>
+        <div>
+          <h1>This is second text</h1>
+          <button className="item-btn">Contact Us</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
